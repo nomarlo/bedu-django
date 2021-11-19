f2059a0fb1a5b3a925eaab74b8af92f87bf44aca
nomarlo <nomarlo_007_noe@hotmail.com>
2021-11-18 19:35:54 -0600

    delete

--- tours.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS Tours;
-CREATE DATABASE Tours;
-
-GRANT ALL PRIVILEGES ON Tours.* TO Tours@'%' IDENTIFIED BY 'Tours';
-
-GRANT ALL PRIVILEGES ON Tours.* TO Tours@'localhost' IDENTIFIED BY 'Tours';
-
-# docker exec -i pythonsql mysql -hlocalhost -uBiblioteca -pBiblioteca Biblioteca < sql/tabla-libro.sql
